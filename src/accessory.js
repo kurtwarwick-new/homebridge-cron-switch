3b4c8794e1d8b2efe34e816c7e8e6ca1f4a96d64
Kurt Warwick <you@example.com>
2020-07-01 09:52:03 +0200

    Inital commit

---
+++ src/accessory.js
@@ -0,0 +1,53 @@
+var cron = require("cron").CronJob;
+
+var Service;
+var Characteristic;
+
+class Accessory {
+    constructor(logger, config, api) {
+        this.configureAccessory = this.configureAccessory.bind(this);
+        this.debug = this.debug.bind(this);
+        this.log = this.log.bind(this);
+
+        this.getServices = this.getServices.bind(this);
+
+        this.config = config;
+        this.logger = logger;
+        this.api = api;
+
+        Service = api.hap.Service;
+        Characteristic = api.hap.Characteristic;
+
+        this.configureAccessory();
+    }
+
+    debug(message) {
+        if (this.config && this.config.debug) {
+            this.logger(message.toLowerCase());
+        }
+    }
+
+    log(message) {
+        this.logger(message.toLowerCase());
+    }
+
+    getServices() {
+        return [this.service];
+    }
+
+    configureAccessory() {
+        this.service = new Service.Switch(`${this.config.name}`);
+
+        let job = new cron(this.config.cron, () => {
+            this.service.updateCharacteristic(Characteristic.On, true);
+            setTimeout(() => this.service.updateCharacteristic(Characteristic.On, false), 100);
+        });
+
+        job.start();
+    }
+}
+
+Accessory.pluginName = "homebridge-cron-switch";
+Accessory.accessoryName = "CronSwitch";
+
+module.exports = Accessory;
